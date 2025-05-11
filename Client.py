cab0e541141c4840fe2129cffbff4bf84a9cdae6
A-Boring-Square <aboringsquare@gmail.com>
2025-05-11 13:55:20 -0500

    the suffering

--- Client.py
+++ Client.py
@@ -3,7 +3,24 @@ import Config
 from Utils import PopupManager
 from NetCode.Networking import UDPClient
 
+from typing import Optional, TypedDict
+
 # Constant: DISCORD_APP_CLIENT_ID
 # The Client ID used for Discord RPC (Rich Presence) integration.
 DISCORD_APP_CLIENT_ID = "13409554293425408734"
 
+pm = PopupManager()
+
+Client = UDPClient.UDPClient(pm.TextInput("Server IP", "Enter the IP of the server you would like to contect to"), 54777)
+
+
+class ClientData(TypedDict):
+    USERNAME: str
+    POS_X: Optional[int]
+    POS_Y: Optional[int]
+    HP: int
+    AMMO: int
+    WEAPON: str
+    STAT_KILLS: int
+    STAT_DEATHS: int
+    
